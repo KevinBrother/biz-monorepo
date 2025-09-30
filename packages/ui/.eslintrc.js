39d42d49277dd2a9533582aaaca1a71bd8f34621
caojunjie <1301239018@qq.com>
2025-09-30 22:13:45 +0800

    fix: ci 3

---
+++ packages/ui/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  extends: ['@biz/eslint-config/react'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  ignorePatterns: [
+    'node_modules/**',
+    'dist/**',
+    'build/**',
+    'storybook-static/**',
+    '.storybook/**',
+  ],
+};
